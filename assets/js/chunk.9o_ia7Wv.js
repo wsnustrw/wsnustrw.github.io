5648ccb3ebcd922c1f7287f6f718f50f6a0938be
seeyoudesign <91014212+seeyoudesign@users.noreply.github.com>
2021-09-24 20:20:18 +0700

    Delete assets directory

--- assets/js/chunk.9o_ia7Wv.js
+++
@@ -1,6 +0,0 @@
-function ab(a, b, c) {
-    return Math.max(b, Math.min(a, c))
-}
-export {
-    ab as a
-};
--- assets/js/chunk.C-y8QDGy.js
+++
@@ -1,8 +0,0 @@
-import {
-    l as U,
-    m as ja
-} from "./chunk.CeGTj4_V.js";
-const qm = U(ja());
-export {
-    qm as a
-};
